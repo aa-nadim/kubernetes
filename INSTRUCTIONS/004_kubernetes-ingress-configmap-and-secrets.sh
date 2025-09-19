2b665ef38efaa7f0b4c212431f2309a3208711d8
unknown <nadim.ice.nstu@gmail.com>
2025-09-19 21:18:02 +0600

    # Ingress controller

--- INSTRUCTIONS/004_kubernetes-ingress-configmap-and-secrets.sh
+++ INSTRUCTIONS/004_kubernetes-ingress-configmap-and-secrets.sh
@@ -24,3 +24,23 @@ kubectl get deploy -n ingress-nginx
 kubectl logs -f deploy/ingress-nginx-controller -n ingress-nginx
 
 kubectl get ingressclass
+
+
+kubectl apply -f ingress.yaml
+
+host example.com
+nslookup example.com
+dig example.com
+
+# For windows
+Resolve-DnsName example.com
+C:\Windows\System32\drivers\etc\hosts
+
+kubectl get configmap -n ingress-nginx
+
+kubectl describe configmap ingress-nginx-controller -n ingress-nginx
+
+
+kubectl exec -it deploy/ingress-nginx-controller -n ingress-nginx -- bash
+ingress-nginx-controller-7df9fc45fd-scnpj:/etc/nginx$ ls
+ingress-nginx-controller-7df9fc45fd-scnpj:/etc/nginx$ cat nginx.conf
